71cf5568e3477e7c2579ea206e795fc4d5c62365
Vitor Filardi <rabelo51@gmail.com>
2020-05-01 16:06:41 -0300

    Alteração da Url para usar api local

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://rocketseat-node.herokuapp.com/api'
+    baseURL: 'http://localhost:3001/api',
 });
-
+// http://rocketseat-node.herokuapp.com/api
 export default api;
